806c7630c45ccbe70d3e8219634c86f285fd25c1
tungwonamz <122849641+tungwonamz@users.noreply.github.com>
2024-04-20 15:45:54 +0800

    Update WebSecurityConfig.java

--- src/main/java/org/owasp/webgoat/container/WebSecurityConfig.java
+++ src/main/java/org/owasp/webgoat/container/WebSecurityConfig.java
@@ -49,7 +49,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-
 /** Security configuration for WebGoat. */
 @Configuration
 @AllArgsConstructor
